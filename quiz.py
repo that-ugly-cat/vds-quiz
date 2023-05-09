587b330a90968db6a48fa99db2a628b7afb3bbbe
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-09 15:12:27 +0200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -3,4 +3,17 @@ import random
 import streamlit as st
 
 domande_df = pd.read_excel('Domande.xlsx')
-st.dataframe(domande_df)
+#st.dataframe(domande_df)
+
+st.header('Quiz volo da diporto e sportivo')
+st.subheader('...')
+
+categories = domande_df['Categoria'].unique().tolist()
+df_dict= {}
+for x in categories:
+    df_name = 'df_' + x
+    df = domande_df[domande_df['Categoria'] == x]
+    df = df.reset_index()
+    df_dict[df_name] = df
+    
+st.write(df_dict)
