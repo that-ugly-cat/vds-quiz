5952b0c9a0c139b3f0a7c06fa8aa86d810fc8413
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-05-09 16:43:06 +0200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -4,7 +4,7 @@ import streamlit as st
 try: 
     import quiz_state
 except:
-    st.text('no state')
+    state = 0
 domande_df = pd.read_excel('Domande.xlsx')
 #st.dataframe(domande_df)
 
@@ -42,16 +42,21 @@ def show_question(question_dict):
      with st.form("my_form"):
         st.subheader(question_dict['question'])
         question_radio = st.radio('La tua risposta', question_dict['answers'])
-        st.session_state['question_radio'] = question_radio
         submitted = st.form_submit_button("Ok")
         if submitted:
-            if st.session_state['question_radio'] == question_dict['c_answer_n']:
-                st.success('Risposta corretta!')
-            else:
-                message = 'Risposta sbagliata. La risposta corretta è:\n' + question_dict['c_answer']
-                st.error(message)
+            with open('quiz_state.py', 'w') as quiz_state_file:
+                quiz_state_file.write('answer_n = ' + question_radio)
+                quiz_state_file.write('c_answer_n = ' + c_answer_n)
+            
     
-
-show_question(get_question(domande_df))
+if state = 0:
+    show_question(get_question(domande_df))
+else:
+    st.write(quiz_state.answer_n)
+    st.write(quiz_state.c_answer_n)
+    if quiz_state.answer_n == quiz_state.c_answer_n:
+        st.success('well done')
+    else:
+        (st.error('Argh')
 st.write(st.session_state)    
 
